c38a5483bd50a7c63c6ac3ea68e5cb75bd6d6d2a
Emre Kilinc <56527956+klncEmre@users.noreply.github.com>
2022-08-16 17:52:48 +0300

    KinFisher added for fethcing images with url.

--- AssingmentView/AssignmentView.swift
+++ AssingmentView/AssignmentView.swift
@@ -7,10 +7,82 @@
 
 import Foundation
 import UIKit
+import Kingfisher
 class AssignmentView: UICollectionView {
+    var imageURLs:[String] = []
+    var readyImages: [UIImageView] = []
     
+    init(){
+        let layout: UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+        layout.itemSize = CGSize(width: 110, height: 110)
+        layout.scrollDirection = .horizontal
+        
+        super.init(frame: CGRect.zero, collectionViewLayout: layout)
+        
+        self.dataSource = self
+        self.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        
+        self.backgroundColor = UIColor.red
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension AssignmentView {
+    public func setImages(images:[String]){
+        self.imageURLs = images
+//        test
+        let customImageView = UIImageView()
+        let url = URL(string: "https://db62cod6cnasq.cloudfront.net/user-media/0/image4-5mb.png")
+        customImageView.kf.indicatorType = .activity
+        customImageView.kf.setImage(
+            with: url,
+            placeholder: UIImage(systemName: "loading" ),
+            options: [
+                .scaleFactor(UIScreen.main.scale),
+                .transition(.fade(1)),
+                .cacheOriginalImage
+            ])
+        {  [weak self]
+            result in
+            switch result {
+            case .success(let value):
+                self?.readyImages.append(customImageView)
+                self!.reloadData()
+                
+                print("Task done for: \(value.source.url?.absoluteString ?? "")")
+            case .failure(let error):
+                print("Job failed: \(error.localizedDescription)")
+            }
+        }
+        //test
+        
+    }
+}
+
+
+extension AssignmentView: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return readyImages.count
+    }
     
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = UIColor.white
+        let imageViewReady = readyImages[indexPath.row]
+        cell.addSubview(imageViewReady)
+      
+        imageViewReady.translatesAutoresizingMaskIntoConstraints = false
+        imageViewReady.widthAnchor.constraint(equalTo: cell.widthAnchor).isActive = true
+        imageViewReady.heightAnchor.constraint(equalTo: cell.heightAnchor ).isActive = true
+        imageViewReady.centerXAnchor.constraint(equalTo: cell.centerXAnchor).isActive = true
+        imageViewReady.centerYAnchor.constraint(equalTo: cell.centerYAnchor).isActive = true
+        
+        return cell
+    }
 }
 
 /**
--- AssingmentView/ViewController.swift
+++ AssingmentView/ViewController.swift
@@ -6,12 +6,44 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import Kingfisher
+class ViewController: UIViewController, UICollectionViewDelegate {
     
+    let imageView = UIImageView()
+    let assignmentView = AssignmentView()
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(assignmentView)
+        assignmentView.translatesAutoresizingMaskIntoConstraints = false
+        assignmentView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        assignmentView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        assignmentView.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+        assignmentView.heightAnchor.constraint(equalToConstant: 200.0 ).isActive = true
+        assignmentView.setImages(images: ["https://db62cod6cnasq.cloudfront.net/user-media/0/image1-500kb.png"])
+        
         // Do any additional setup after loading the view.
     }
 }
 
+//imageView.translatesAutoresizingMaskIntoConstraints = false
+//        imageView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+//        imageView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+//        let url = URL(string: "https://db62cod6cnasq.cloudfront.net/user-media/0/image4-5mb.png")
+//        imageView.kf.indicatorType = .activity
+//        imageView.kf.setImage(
+//            with: url,
+//            placeholder: UIImage(systemName: "loading" ),
+//            options: [
+//                .scaleFactor(UIScreen.main.scale),
+//                .transition(.fade(1)),
+//                .cacheOriginalImage
+//            ])
+//        {
+//            result in
+//            switch result {
+//            case .success(let value):
+//                print("Task done for: \(value.source.url?.absoluteString ?? "")")
+//            case .failure(let error):
+//                print("Job failed: \(error.localizedDescription)")
+//            }
+//        }
